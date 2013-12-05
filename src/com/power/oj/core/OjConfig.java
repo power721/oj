f97e20fa963d640d376f65f9a88ab3982eba4635
魔神翼 <power721@163.com>
2013-12-05 21:50:59 +0800

    Replace c3p0 with druid.

--- src/com/power/oj/core/OjConfig.java
+++ src/com/power/oj/core/OjConfig.java
@@ -24,25 +24,29 @@ import com.power.oj.solution.SolutionController;
 import com.power.oj.solution.SolutionModel;
 import com.power.oj.user.*;
 import com.power.oj.util.Tool;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.wall.WallFilter;
 import com.jfinal.config.*;
 import com.jfinal.core.JFinal;
 import com.jfinal.ext.interceptor.SessionInViewInterceptor;
 import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
-import com.jfinal.plugin.c3p0.C3p0Plugin;
+import com.jfinal.plugin.druid.DruidPlugin;
+import com.jfinal.plugin.druid.DruidStatViewHandler;
 
 /**
  * Configure the system.
+ * 
  * @author power
- *
+ * 
  */
 public class OjConfig extends JFinalConfig
 {
 	protected final Logger log = Logger.getLogger(getClass());
-	
+
 	public static String baseUrl = null;
 	public static String siteTitle = "Power OJ";
-	
+
 	public static List<LanguageModel> program_languages;
 	public static IntHashMap language_type = new IntHashMap();
 	public static IntHashMap language_name = new IntHashMap();
@@ -54,10 +58,9 @@ public class OjConfig extends JFinalConfig
 	public static int contestPageSize = 20;
 	public static int problemPageSize = 50;
 	public static int userPageSize = 20;
-	
+
 	public static long startGlobalInterceptorTime;
 	public static long startGlobalHandlerTime;
-
 
 	/**
 	 * 配置常量
@@ -66,12 +69,12 @@ public class OjConfig extends JFinalConfig
 	{
 		// 加载少量必要配置，随后可用getProperty(...)获取值
 		loadPropertyFile("oj.properties");
-		
+
 		me.setDevMode(getPropertyToBoolean("devMode", false));
 		me.setBaseViewPath("/WEB-INF/pages");
 		me.setError404View("/WEB-INF/pages/common/404.html");
 		me.setError500View("/WEB-INF/pages/common/500.html");
-		
+
 		log.debug("configConstant finished.");
 	}
 
@@ -88,7 +91,7 @@ public class OjConfig extends JFinalConfig
 		me.add("/problem", ProblemController.class);
 		me.add("/solution", SolutionController.class);
 		me.add("/user", UserController.class);
-		
+
 		log.debug("configRoute finished.");
 	}
 
@@ -98,21 +101,30 @@ public class OjConfig extends JFinalConfig
 	public void configPlugin(Plugins me)
 	{
 		// 配置C3p0数据库连接池插件
-		C3p0Plugin c3p0Plugin = new C3p0Plugin(getProperty("jdbcUrl"), getProperty("user"), getProperty("password")
-				.trim());
-		me.add(c3p0Plugin);
+		/*
+		 * C3p0Plugin c3p0Plugin = new C3p0Plugin(getProperty("jdbcUrl"),
+		 * getProperty("user"), getProperty("password") .trim());
+		 * me.add(c3p0Plugin);
+		 */
+		DruidPlugin dp = new DruidPlugin(getProperty("jdbcUrl"), getProperty("user"), getProperty("password").trim());
+		dp.addFilter(new StatFilter());
+		WallFilter wall = new WallFilter();
+		wall.setDbType("mysql");
+		dp.addFilter(wall);
+		me.add(dp);
 
 		// 配置ActiveRecord插件
-		ActiveRecordPlugin arp = new ActiveRecordPlugin(c3p0Plugin);
+		ActiveRecordPlugin arp = new ActiveRecordPlugin(dp);
 		arp.setShowSql(true);
-		arp.addMapping("user", "uid", UserModel.class); // 映射user表到 User模型, 主键是uid
+		arp.addMapping("user", "uid", UserModel.class); // 映射user表到 User模型,
+														// 主键是uid
 		arp.addMapping("problem", "pid", ProblemModel.class);
 		arp.addMapping("solution", "sid", SolutionModel.class);
 		arp.addMapping("contest", "cid", ContestModel.class);
 		arp.addMapping("program_language", LanguageModel.class);
 		arp.addMapping("variable", VariableModel.class);
 		me.add(arp);
-		
+
 		log.debug("configPlugin finished.");
 	}
 
@@ -127,7 +139,7 @@ public class OjConfig extends JFinalConfig
 		me.add(new MessageInterceptor());
 		me.add(new AccessLogInterceptor());
 		me.add(new UserInterceptor());
-		
+
 		log.debug("configInterceptor finished.");
 	}
 
@@ -137,7 +149,9 @@ public class OjConfig extends JFinalConfig
 	public void configHandler(Handlers me)
 	{
 		me.add(new UrlFiterHandler());
-		
+		DruidStatViewHandler dvh = new DruidStatViewHandler("/druid");
+		me.add(dvh);
+
 		log.debug("configHandler finished.");
 	}
 
@@ -149,13 +163,13 @@ public class OjConfig extends JFinalConfig
 		baseUrl = Tool.formatBaseURL(getProperty(OjConstants.BASE_URL));
 		siteTitle = getProperty(OjConstants.SITE_TITLE, "Power OJ");
 		init();
-		
+
 		log.debug("afterJFinalStart finished.");
 	}
 
 	/**
-	 * Initialize configuration form DB.
-	 * Initialize the programming language and judge result type.
+	 * Initialize configuration form DB. Initialize the programming language and
+	 * judge result type.
 	 */
 	public static void init()
 	{
@@ -190,7 +204,7 @@ public class OjConfig extends JFinalConfig
 			result_type.put(resultType.getId(), resultType);
 		}
 	}
-	
+
 	/*
 	 * get OJ variable from DB cache
 	 */
@@ -218,11 +232,11 @@ public class OjConfig extends JFinalConfig
 	{
 		return variable.get(name).getStr("type");
 	}
-	
+
 	/**
 	 * 建议使用 JFinal 手册推荐的方式启动项目 运行此 main
-	 * 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
-	 * 使用内置的Jetty容器， 基于Tomcat开发，需要将jetty.jar删除
+	 * 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此 使用内置的Jetty容器，
+	 * 基于Tomcat开发，需要将jetty.jar删除
 	 */
 	public static void main(String[] args)
 	{
