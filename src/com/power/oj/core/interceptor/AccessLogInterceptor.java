b20a1fcfe420bf362c93277c5da23e12e56dde53
power <power721@163.com>
2013-12-06 13:58:13 +0800

    Delete the url with scheme.

--- src/com/power/oj/core/interceptor/AccessLogInterceptor.java
+++ src/com/power/oj/core/interceptor/AccessLogInterceptor.java
@@ -1,8 +1,5 @@
 package com.power.oj.core.interceptor;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
 import javax.servlet.http.HttpSession;
 
 import jodd.util.StringBand;
@@ -16,7 +13,7 @@ import com.jfinal.plugin.activerecord.Db;
 import com.power.oj.core.OjConfig;
 
 /**
- * Update session table with uri and timestamp.
+ * Update session table with url and timestamp.
  * 
  * @author power
  * 
@@ -33,35 +30,24 @@ public class AccessLogInterceptor implements Interceptor
 		Controller controller = ai.getController();
 		HttpSession session = controller.getSession(true);
 		String actionKey = ai.getActionKey();
-		String url;
 		
 		if (StringUtil.equalsOne(actionKey, skipActions) == -1)
 		{
-			StringBand rsb = new StringBand(actionKey);
+			StringBand sb = new StringBand(actionKey);
 			if (controller.getPara() != null)
-				rsb.append("/").append(controller.getPara());
+				sb.append("/").append(controller.getPara());
 
-			StringBand sb = new StringBand(controller.getRequest().getRequestURI());
 			String query = controller.getRequest().getQueryString();
 			if (query != null)
 			{
 				sb.append("?").append(query);
-				rsb.append("?").append(query);
 			}
 			
-			url = rsb.toString();
+			String url = sb.toString();
 			if (url.indexOf("ajax=1") == -1)
 				OjConfig.lastURL = url;
-			
-			try
-			{
-				controller.setAttr("uri", URLEncoder.encode(sb.toString(), "UTF-8"));
-			} catch (UnsupportedEncodingException e)
-			{
-				log.error(e.getLocalizedMessage());
-			}
 
-			Db.update("UPDATE session SET last_activity=UNIX_TIMESTAMP(),uri=? WHERE session_id=?", sb.toString(), session.getId());
+			Db.update("UPDATE session SET last_activity=UNIX_TIMESTAMP(),uri=? WHERE session_id=?", url, session.getId());
 		}
 
 		ai.invoke();
