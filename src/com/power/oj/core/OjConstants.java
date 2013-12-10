b4c6af51eb3e5e8ad5efd912522f03e631a505f4
魔神翼 <power721@163.com>
2013-12-10 22:40:12 +0800

    Update OjConstants.

--- src/com/power/oj/core/OjConstants.java
+++ src/com/power/oj/core/OjConstants.java
@@ -8,10 +8,9 @@ package com.power.oj.core;
  */
 public abstract class OjConstants
 {
-  /**
+  /*
    * These contants are used in view(attr name) and session.
    */
-  public final static String randomCodeKey = "PowerOJCaptcha";
 
   public final static String SITE_TITLE = "siteTitle";
   public final static String PAGE_TITLE = "pageTitle";
@@ -28,12 +27,18 @@ public abstract class OjConstants
   public final static String USER_LIST = "userList";
   public final static String ADMIN_USER = "adminUser";
 
-  public final static int TOKEN_AGE = 3600 * 24 * 7;
-  public final static String TOKEN_NAME = "name";
-  public final static String TOKEN_TOKEN = "token";
-
   public final static String PROGRAM_LANGUAGES = "program_languages";
   public final static String LANGUAGE_NAME = "language_name";
   public final static String JUDGE_RESULT = "judge_result";
   public final static String RESULT_TYPE = "result_type";
+
+  /*
+   * These contants are used in cookie.
+   */
+  public final static String randomCodeKey = "PowerOJCaptcha";
+  
+  public final static int TOKEN_AGE = 3600 * 24 * 7;
+  public final static String TOKEN_NAME = "PowerOjName";
+  public final static String TOKEN_TOKEN = "PowerOjToken";
+
 }
