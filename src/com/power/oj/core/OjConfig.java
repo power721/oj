f1009bed5808c5e9eada7f10e093c979ebbad20b
魔神翼 <power721@163.com>
2014-01-02 22:06:49 +0800

    move some methods from OjService to OjConfig.

--- src/com/power/oj/core/OjConfig.java
+++ src/com/power/oj/core/OjConfig.java
@@ -1,11 +1,14 @@
 package com.power.oj.core;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import com.power.oj.core.bean.ResultType;
 import com.power.oj.core.model.LanguageModel;
 import com.power.oj.core.model.VariableModel;
+import com.power.oj.util.FileKit;
 
 import jodd.util.collection.IntHashMap;
 
@@ -42,6 +45,70 @@ public class OjConfig
   public static long timeStamp;
   public static long startGlobalInterceptorTime;
   public static long startGlobalHandlerTime;
+
+  public static void loadVariable()
+  {
+    variable = new HashMap<String, VariableModel>();
+    for (VariableModel variableModel : VariableModel.dao.find("SELECT * FROM variable"))
+    {
+      variable.put(variableModel.getStr("name"), variableModel);
+    }
+    
+    siteTitle = get("siteTitle", "Power OJ");
+
+    String uploadPath = get("uploadPath", "upload/");
+    uploadPath = FileKit.parsePath(uploadPath);
+
+    String downloadPath = get("downloadPath", "download/");
+    downloadPath = FileKit.parsePath(downloadPath);
+    
+    String userAvatarPath = get("userAvatarPath", "assets/images/user/");
+    userAvatarPath = FileKit.parsePath(userAvatarPath);
+    
+    String problemImagePath = get("problemImagePath", "assets/images/problem/");
+    problemImagePath = FileKit.parsePath(problemImagePath);
+    
+    contestPageSize = getInt("contestPageSize", 20);
+    contestRankPageSize = getInt("contestRankPageSize", 50);
+    problemPageSize = getInt("problemPageSize", 50);
+    userPageSize = getInt("userPageSize", 20);
+    statusPageSize = getInt("statusPageSize", 20);
+  }
+  
+  public static void loadLanguage()
+  {
+    language_type = new IntHashMap();
+    language_name = new IntHashMap();
+    program_languages = LanguageModel.dao.find("SELECT * FROM program_language WHERE status=1");
+    for (LanguageModel Language : program_languages)
+    {
+      language_type.put(Language.getInt("id"), Language);
+      language_name.put(Language.getInt("id"), Language.getStr("name"));
+    }
+  }
+  
+  public static void initJudgeResult()
+  {
+    judge_result = new ArrayList<ResultType>();
+    judge_result.add(new ResultType(ResultType.AC, "AC", "Accepted"));
+    judge_result.add(new ResultType(ResultType.PE, "PE", "Presentation Error"));
+    judge_result.add(new ResultType(ResultType.TLE, "TLE", "Time Limit Exceed"));
+    judge_result.add(new ResultType(ResultType.MLE, "MLE", "Memory Limit Exceed"));
+    judge_result.add(new ResultType(ResultType.WA, "WA", "Wrong Answer"));
+    judge_result.add(new ResultType(ResultType.RE, "RE", "Runtime Error"));
+    judge_result.add(new ResultType(ResultType.OLE, "OLE", "Output Limit Exceed"));
+    judge_result.add(new ResultType(ResultType.CE, "CE", "Compile Error"));
+    judge_result.add(new ResultType(ResultType.SE, "SE", "System Error"));
+    judge_result.add(new ResultType(ResultType.VE, "VE", "Validate Error"));
+    judge_result.add(new ResultType(ResultType.Wait, "Wait", "Waiting"));
+
+    result_type = new IntHashMap();
+    for (Iterator<ResultType> it = judge_result.iterator(); it.hasNext();)
+    {
+      ResultType resultType = it.next();
+      result_type.put(resultType.getId(), resultType);
+    }
+  }
 
   /*
    * get OJ variable from DB cache
--- src/com/power/oj/core/service/OjService.java
+++ src/com/power/oj/core/service/OjService.java
@@ -1,22 +1,14 @@
 package com.power.oj.core.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
 import jodd.mail.EmailMessage;
 import jodd.util.MimeTypes;
-import jodd.util.collection.IntHashMap;
-
 import com.jfinal.log.Logger;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Record;
+
 import com.power.oj.core.OjConfig;
-import com.power.oj.core.bean.ResultType;
-import com.power.oj.core.model.LanguageModel;
-import com.power.oj.core.model.VariableModel;
-import com.power.oj.util.FileKit;
 import com.power.oj.util.Tool;
 
 public class OjService
@@ -36,66 +28,17 @@ public class OjService
   
   public void loadVariable()
   {
-    OjConfig.variable = new HashMap<String, VariableModel>();
-    for (VariableModel variableModel : VariableModel.dao.find("SELECT * FROM variable"))
-    {
-      OjConfig.variable.put(variableModel.getStr("name"), variableModel);
-    }
-    
-    OjConfig.siteTitle = OjConfig.get("siteTitle", "Power OJ");
-
-    String uploadPath = OjConfig.get("uploadPath", "upload/");
-    OjConfig.uploadPath = FileKit.parsePath(uploadPath);
-
-    String downloadPath = OjConfig.get("downloadPath", "download/");
-    OjConfig.downloadPath = FileKit.parsePath(downloadPath);
-    
-    String userAvatarPath = OjConfig.get("userAvatarPath", "assets/images/user/");
-    OjConfig.userAvatarPath = FileKit.parsePath(userAvatarPath);
-    
-    String problemImagePath = OjConfig.get("problemImagePath", "assets/images/problem/");
-    OjConfig.problemImagePath = FileKit.parsePath(problemImagePath);
-    
-    OjConfig.contestPageSize = OjConfig.getInt("contestPageSize", 20);
-    OjConfig.contestRankPageSize = OjConfig.getInt("contestRankPageSize", 50);
-    OjConfig.problemPageSize = OjConfig.getInt("problemPageSize", 50);
-    OjConfig.userPageSize = OjConfig.getInt("userPageSize", 20);
-    OjConfig.statusPageSize = OjConfig.getInt("statusPageSize", 20);
+    OjConfig.loadVariable();
   }
   
   public void loadLanguage()
   {
-    OjConfig.language_type = new IntHashMap();
-    OjConfig.language_name = new IntHashMap();
-    OjConfig.program_languages = LanguageModel.dao.find("SELECT * FROM program_language WHERE status=1");
-    for (LanguageModel Language : OjConfig.program_languages)
-    {
-      OjConfig.language_type.put(Language.getInt("id"), Language);
-      OjConfig.language_name.put(Language.getInt("id"), Language.getStr("name"));
-    }
+    OjConfig.loadLanguage();
   }
   
   public void initJudgeResult()
   {
-    OjConfig.judge_result = new ArrayList<ResultType>();
-    OjConfig.judge_result.add(new ResultType(ResultType.AC, "AC", "Accepted"));
-    OjConfig.judge_result.add(new ResultType(ResultType.PE, "PE", "Presentation Error"));
-    OjConfig.judge_result.add(new ResultType(ResultType.TLE, "TLE", "Time Limit Exceed"));
-    OjConfig.judge_result.add(new ResultType(ResultType.MLE, "MLE", "Memory Limit Exceed"));
-    OjConfig.judge_result.add(new ResultType(ResultType.WA, "WA", "Wrong Answer"));
-    OjConfig.judge_result.add(new ResultType(ResultType.RE, "RE", "Runtime Error"));
-    OjConfig.judge_result.add(new ResultType(ResultType.OLE, "OLE", "Output Limit Exceed"));
-    OjConfig.judge_result.add(new ResultType(ResultType.CE, "CE", "Compile Error"));
-    OjConfig.judge_result.add(new ResultType(ResultType.SE, "SE", "System Error"));
-    OjConfig.judge_result.add(new ResultType(ResultType.VE, "VE", "Validate Error"));
-    OjConfig.judge_result.add(new ResultType(ResultType.Wait, "Wait", "Waiting"));
-
-    OjConfig.result_type = new IntHashMap();
-    for (Iterator<ResultType> it = OjConfig.judge_result.iterator(); it.hasNext();)
-    {
-      ResultType resultType = it.next();
-      OjConfig.result_type.put(resultType.getId(), resultType);
-    }
+    OjConfig.initJudgeResult();
   }
 
   public boolean sendResetPasswordEmail(String name, String email, String token) throws Exception
