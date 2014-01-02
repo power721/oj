289a87442e81c5353e0f44d228e8dc1ffa0187fc
魔神翼 <power721@163.com>
2014-01-02 21:30:53 +0800

    change OjConstants to interface.

--- src/com/power/oj/core/OjConstants.java
+++ src/com/power/oj/core/OjConstants.java
@@ -6,50 +6,50 @@ package com.power.oj.core;
  * @author power
  * 
  */
-public abstract class OjConstants
+public interface OjConstants
 {
   /*
    * These contants are used in general.
    */
-  public final static long SessionExpiresTime = 15 * 60 * 1000;
-  public final static long resetPasswordExpiresTime = 30 * 60 * 1000;
+  long SessionExpiresTime = 15 * 60 * 1000;
+  long resetPasswordExpiresTime = 30 * 60 * 1000;
   
   /*
    * These contants are used in view(attr name) and session.
    */
 
-  public final static String SITE_TITLE = "siteTitle";
-  public final static String PAGE_TITLE = "pageTitle";
-  public final static String BASE_URL = "baseUrl";
+  String SITE_TITLE = "siteTitle";
+  String PAGE_TITLE = "pageTitle";
+  String BASE_URL = "baseUrl";
   
-  public final static String CONTROLLER_KEY = "controllerKey";
-  public final static String ACTION_KEY = "actionKey";
-  public final static String METHOD_NAME = "methodName";
+  String CONTROLLER_KEY = "controllerKey";
+  String ACTION_KEY = "actionKey";
+  String METHOD_NAME = "methodName";
 
-  public final static String MSG = "msg";
-  public final static String MSG_TYPE = "msgType";
-  public final static String MSG_TITLE = "msgTitle";
+  String MSG = "msg";
+  String MSG_TYPE = "msgType";
+  String MSG_TITLE = "msgTitle";
 
-  public final static String USER = "user";
-  public final static String USER_ID = "userID";
-  public final static String USER_NAME = "userName";
-  public final static String USER_EMAIL = "userEmail";
-  public final static String USER_LIST = "userList";
-  public final static String ADMIN_USER = "adminUser";
+  String USER = "user";
+  String USER_ID = "userID";
+  String USER_NAME = "userName";
+  String USER_EMAIL = "userEmail";
+  String USER_LIST = "userList";
+  String ADMIN_USER = "adminUser";
 
-  public final static String PROGRAM_LANGUAGES = "program_languages";
-  public final static String LANGUAGE_NAME = "language_name";
-  public final static String JUDGE_RESULT = "judge_result";
-  public final static String RESULT_TYPE = "result_type";
+  String PROGRAM_LANGUAGES = "program_languages";
+  String LANGUAGE_NAME = "language_name";
+  String JUDGE_RESULT = "judge_result";
+  String RESULT_TYPE = "result_type";
   
-  public final static String LAST_ACCESS_URL = "lastAccessURL";
+  String LAST_ACCESS_URL = "lastAccessURL";
   /*
    * These contants are used in cookie.
    */
-  public final static String randomCodeKey = "PowerOJCaptcha";
+  String randomCodeKey = "PowerOJCaptcha";
   
-  public final static int TOKEN_AGE = 3600 * 24 * 7;
-  public final static String TOKEN_NAME = "PowerOjName";
-  public final static String TOKEN_TOKEN = "PowerOjToken";
+  int TOKEN_AGE = 3600 * 24 * 7;
+  String TOKEN_NAME = "PowerOjName";
+  String TOKEN_TOKEN = "PowerOjToken";
 
 }
