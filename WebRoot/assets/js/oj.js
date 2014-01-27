58f60f7613ffbfaf9038129063bd3d03a44cc8ec
power <power721@163.com>
2014-01-27 10:08:36 +0800

    delay hover event to get user info.

--- WebRoot/assets/js/oj.js
+++ WebRoot/assets/js/oj.js
@@ -1,13 +1,45 @@
-/*
 $(document).ready(function() {
-	$('td.user a').mouseover(function() {
-		var name = $(this).html();
-		$.getJSON('user/info', {'name':name}, function(data) {
-			
+	$('td.user a').each(function() {
+		var that = $(this);
+		var name = that.html();
+		that.hoverDelay({
+			hoverEvent : function() {
+				$.getJSON('user/info', {
+					'name' : name
+				}, function(data) {
+					;
+				});
+			}
 		});
 	});
 });
-*/
+
+(function($) {
+	$.fn.hoverDelay = function(options) {
+		var defaults = {
+			hoverDuring : 500,
+			outDuring : 500,
+			hoverEvent : function() {
+				$.noop();
+			},
+			outEvent : function() {
+				$.noop();
+			}
+		};
+		var sets = $.extend(defaults, options || {});
+		var hoverTimer, outTimer;
+		return $(this).each(function() {
+			$(this).hover(function() {
+				clearTimeout(outTimer);
+				hoverTimer = setTimeout(sets.hoverEvent, sets.hoverDuring);
+			}, function() {
+				clearTimeout(hoverTimer);
+				outTimer = setTimeout(sets.outEvent, sets.outDuring);
+			});
+		});
+	};
+})(jQuery);
+
 Date.prototype.format = function(format) {
 	var o = {
 		"M+" : this.getMonth() + 1, // month
@@ -20,13 +52,16 @@ Date.prototype.format = function(format) {
 	// millisecond
 	};
 	if (/(y+)/.test(format))
-		format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+		format = format.replace(RegExp.$1, (this.getFullYear() + "")
+				.substr(4 - RegExp.$1.length));
 	for ( var k in o)
 		if (new RegExp("(" + k + ")").test(format))
-			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k]
+					: ("00" + o[k]).substr(("" + o[k]).length));
 	return format;
 };
 
 function getLocalTime(nS) {
-	return new Date(parseInt(nS) * 1000).toLocaleString().replace(/ Years | Month /g, "-").replace(/ Day /g, " ");
+	return new Date(parseInt(nS) * 1000).toLocaleString().replace(
+			/ Years | Month /g, "-").replace(/ Day /g, " ");
 }
