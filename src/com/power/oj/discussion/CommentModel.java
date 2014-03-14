f6c9f275bc12850baa544bd4d654df3d5d146cfb
power721 <power721@163.com>
2014-03-14 21:04:55 +0800

    update discussion model.

--- src/com/power/oj/discussion/CommentModel.java
+++ src/com/power/oj/discussion/CommentModel.java
@@ -4,11 +4,86 @@ import com.jfinal.plugin.activerecord.Model;
 
 public class CommentModel extends Model<CommentModel>
 {
+  private static final long serialVersionUID = 1L;
+  
+  public static final CommentModel dao = new CommentModel();
+  
+  public static final String ID = "id";
+  public static final String UID = "uid";
+  public static final String THREAD_ID = "threadId";
+  public static final String QUOTE_ID = "quoteId";
+  public static final String CONTENT = "content";
+  public static final String IP = "ip";
+  public static final String CTIME = "ctime";
 
-  /**
-   * 
-   */
-  private static final long serialVersionUID = 618379082199305002L;
-
-  public static CommentModel dao = new CommentModel();
+  public Integer getId()
+  {
+    return getInt(ID);
+  }
+  
+  public CommentModel setId(Integer value)
+  {
+    return set(ID, value);
+  }
+  
+  public Integer getUid()
+  {
+    return getInt(UID);
+  }
+  
+  public CommentModel setUid(Integer value)
+  {
+    return set(UID, value);
+  }
+  
+  public Integer getThreadId()
+  {
+    return getInt(THREAD_ID);
+  }
+  
+  public CommentModel setThreadId(Integer value)
+  {
+    return set(THREAD_ID, value);
+  }
+  
+  public Integer getQuoteId()
+  {
+    return getInt(QUOTE_ID);
+  }
+  
+  public CommentModel setQuoteId(Integer value)
+  {
+    return set(QUOTE_ID, value);
+  }
+  
+  public String getContent()
+  {
+    return getStr(CONTENT);
+  }
+  
+  public CommentModel setContent(String value)
+  {
+    return set(CONTENT, value);
+  }
+  
+  public String getIp()
+  {
+    return getStr(IP);
+  }
+  
+  public CommentModel setIp(String value)
+  {
+    return set(IP, value);
+  }
+  
+  public Integer getCtime()
+  {
+    return getInt(CTIME);
+  }
+  
+  public CommentModel setCtime(Integer value)
+  {
+    return set(CTIME, value);
+  }
+  
 }
--- src/com/power/oj/discussion/TopicModel.java
+++ src/com/power/oj/discussion/TopicModel.java
@@ -4,9 +4,108 @@ import com.jfinal.plugin.activerecord.Model;
 
 public class TopicModel extends Model<TopicModel>
 {
-
-  private static final long serialVersionUID = -8030493959732038767L;
+  private static final long serialVersionUID = 1L;
   
   public static final TopicModel dao = new TopicModel();
+  
+  public static final String ID = "id";
+  public static final String UID = "uid";
+  public static final String PID = "pid";
+  public static final String TITLE = "title";
+  public static final String CONTENT = "content";
+  public static final String ATIME = "atime";
+  public static final String CTIME = "ctime";
+  public static final String MTIME = "mtime";
+  public static final String STATUS = "status";
+
+  public Integer getId()
+  {
+    return getInt(ID);
+  }
+  
+  public TopicModel setId(Integer value)
+  {
+    return set(ID, value);
+  }
+  
+  public Integer getUid()
+  {
+    return getInt(UID);
+  }
+  
+  public TopicModel setUid(Integer value)
+  {
+    return set(UID, value);
+  }
+  
+  public Integer getPid()
+  {
+    return getInt(PID);
+  }
+  
+  public TopicModel setPid(Integer value)
+  {
+    return set(PID, value);
+  }
+  
+  public String getTitle()
+  {
+    return getStr(TITLE);
+  }
+  
+  public TopicModel setTitle(String value)
+  {
+    return set(TITLE, value);
+  }
+  
+  public String getContent()
+  {
+    return getStr(CONTENT);
+  }
+  
+  public TopicModel setContent(String value)
+  {
+    return set(CONTENT, value);
+  }
+  
+  public Integer getAtime()
+  {
+    return getInt(ATIME);
+  }
+  
+  public TopicModel setAtime(Integer value)
+  {
+    return set(ATIME, value);
+  }
+  
+  public Integer getCtime()
+  {
+    return getInt(CTIME);
+  }
+  
+  public TopicModel setCtime(Integer value)
+  {
+    return set(CTIME, value);
+  }
+  
+  public Integer getMtime()
+  {
+    return getInt(MTIME);
+  }
+  
+  public TopicModel setMtime(Integer value)
+  {
+    return set(MTIME, value);
+  }
+  
+  public Boolean getStatus()
+  {
+    return getBoolean(STATUS);
+  }
+  
+  public TopicModel setStatus(Boolean value)
+  {
+    return set(STATUS, value);
+  }
   
 }
