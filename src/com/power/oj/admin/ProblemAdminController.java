0644ad720cad8990fd621daab22dca220c971d8e
power721 <power721@163.com>
2014-04-13 12:45:03 +0800

    add access key for submit form.

--- src/com/power/oj/admin/ProblemAdminController.java
+++ src/com/power/oj/admin/ProblemAdminController.java
@@ -69,7 +69,7 @@ public class ProblemAdminController extends OjController
         FlashMessage msg = new FlashMessage(getText("problem.save.warn"), MessageType.WARN, getText("message.warn.title"));
         setFlashMessage(msg);
       }
-      redirectURL = new StringBuilder(2).append("/admin/problem/show/").append(problemModel.getPid()).toString();
+      redirectURL = new StringBuilder(2).append("/admin/problem/data/").append(problemModel.getPid()).toString();
     } catch (IOException e)
     {
       if (OjConfig.getDevMode())
