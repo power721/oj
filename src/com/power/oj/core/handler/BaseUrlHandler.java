fbdb595e244ea669870a5ab402e5f99196ec2a50
power <power0721@gmail.com>
2014-12-13 14:16:21 +0800

    fix base url, need test for IE8+

--- src/com/power/oj/core/handler/BaseUrlHandler.java
+++ src/com/power/oj/core/handler/BaseUrlHandler.java
@@ -8,27 +8,24 @@ import jodd.util.StringUtil;
 import com.power.oj.core.OjConfig;
 import com.power.oj.core.OjConstants;
 
-public class BaseUrlHandler extends Handler
-{
+public class BaseUrlHandler extends Handler {
 
-  @Override
-  public void handle(String target, HttpServletRequest request, HttpServletResponse response, boolean[] isHandled)
-  {
-    if (StringUtil.isBlank(OjConfig.getBaseURL()))
-    {
-      StringBuilder sb = new StringBuilder().append(request.getScheme()).append("://").append(request.getServerName());
-      
-      if (request.getServerPort() != 80)
-      {
-        sb.append(':').append(request.getServerPort());
-      }
-      sb.append(request.getContextPath());
-  
-      OjConfig.setBaseURL(sb.toString());
-    }
-    
-    request.setAttribute(OjConstants.BASE_URL, OjConfig.getBaseURL());
-    nextHandler.handle(target, request, response, isHandled);
-  }
+	@Override
+	public void handle(String target, HttpServletRequest request, HttpServletResponse response, boolean[] isHandled) {
+		if (StringUtil.isBlank(OjConfig.getBaseURL())) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(request.getScheme()).append("://").append(request.getServerName());
+
+			if (request.getServerPort() != 80) {
+				sb.append(':').append(request.getServerPort());
+			}
+			sb.append(request.getContextPath());
+
+			OjConfig.setBaseURL(sb.toString());
+		}
+
+		request.setAttribute(OjConstants.BASE_URL, request.getContextPath());
+		nextHandler.handle(target, request, response, isHandled);
+	}
 
 }
