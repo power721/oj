f434aa3365cb2043d40eb6acf85e4caaad4eb622
power <power0721@gmail.com>
2016-07-09 14:52:29 +0800

    split Routes

---
+++ src/main/java/com/power/oj/core/AdminRoutes.java
@@ -0,0 +1,17 @@
+package com.power.oj.core;
+
+import com.jfinal.config.Routes;
+import com.power.oj.admin.AdminController;
+import com.power.oj.admin.ContestAdminController;
+import com.power.oj.admin.ProblemAdminController;
+import com.power.oj.admin.UserAdminController;
+
+public class AdminRoutes extends Routes {
+    @Override
+    public void config() {
+        add("/admin", AdminController.class);
+        add("/admin/contest", ContestAdminController.class);
+        add("/admin/problem", ProblemAdminController.class);
+        add("/admin/user", UserAdminController.class);
+    }
+}
