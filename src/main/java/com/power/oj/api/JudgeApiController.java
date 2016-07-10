14e51a430dca5f8b7f8169e6eec4950381ccac6f
power <power0721@gmail.com>
2016-07-10 10:20:27 +0800

    add class JudgeResult

--- src/main/java/com/power/oj/api/JudgeApiController.java
+++ src/main/java/com/power/oj/api/JudgeApiController.java
@@ -6,6 +6,7 @@ import com.jfinal.log.Logger;
 import com.jfinal.upload.UploadFile;
 import com.power.oj.core.OjController;
 import com.power.oj.core.bean.ResultType;
+import com.power.oj.judge.JudgeResult;
 import com.power.oj.solution.SolutionService;
 
 import java.io.File;
@@ -21,22 +22,21 @@ public class JudgeApiController extends OjController {
         if (uploadFile != null) {
             file = uploadFile.getFile();
         }
-        String token = getPara("token");
         int result = getParaToInt("result");
         int sid = getParaToInt("sid");
         int cid = getParaToInt("cid");
-        int time = 0;
-        int memory = 0;
-        int test = 0;
+        JudgeResult judgeResult = new JudgeResult(sid, cid, result);
+        judgeResult.setToken(getPara("token"));
+        judgeResult.readError(file);
 
         if (result != ResultType.CE && result != ResultType.SE) {
-            time = getParaToInt("time");
-            memory = getParaToInt("memory");
-            test = getParaToInt("test");
+            judgeResult.setTime(getParaToInt("time"));
+            judgeResult.setMemory(getParaToInt("memory"));
+            judgeResult.setTest(getParaToInt("test"));
         }
 
-        LOGGER.info("sid=" + sid + " cid=" + cid + " result=" + result + " time=" + time+ " memory=" + memory + " test=" + test);
-        if(SolutionService.me().setResult(sid, cid, result, time, memory, test, token, file)) {
+        LOGGER.info(judgeResult.toString());
+        if(SolutionService.me().setResult(judgeResult)) {
             LOGGER.debug("set result successfully.");
         } else {
             LOGGER.error("set result failed!");
