142ea2ac0633005f620fdfb652d9dff470218cb3
power <power0721@gmail.com>
2016-07-10 11:24:38 +0800

    update message

--- src/main/java/com/power/oj/solution/SolutionService.java
+++ src/main/java/com/power/oj/solution/SolutionService.java
@@ -39,7 +39,7 @@ public final class SolutionService {
     public static void checkCompileError(Solution solution, String error) {
         if (error.contains(OjConfig.getString("dataPath")) || error.contains(OjConfig.getString("workPath"))) {
             String message =
-                "User " + solution.getUid() + " from " + SessionService.me().getHost() + " try to hack system!";
+                "User id " + solution.getUid() + " from " + SessionService.me().getHost() + " try to hack system!";
             LOGGER.warn(message);
             solution.setSystemError(message + "\n" + error);
         } else {
