08edc8fa50cb1a78c3b0fdf06921e88060bfb978
上决╇ф <w703710691d@163.com.com>
2017-06-19 20:10:56 +0800

    修正错误

--- src/main/java/com/power/oj/cprogram/CProgramRoutes.java
+++ src/main/java/com/power/oj/cprogram/CProgramRoutes.java
@@ -4,11 +4,7 @@ package com.power.oj.cprogram;
  * Created by w703710691d on 2017/6/14.
  */
 import com.jfinal.config.Routes;
-import com.power.oj.cprogram.admin.AdminController;
-import com.power.oj.cprogram.course_exam.CourseExameController;
-import com.power.oj.cprogram.experiment.ExperimentController;
-import com.power.oj.cprogram.experiment_exam.ExperimentExamController;
-import com.power.oj.cprogram.work.WorkController;
+
 
 public class CProgramRoutes extends Routes {
     @Override
