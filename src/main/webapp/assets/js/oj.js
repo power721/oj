d3c5016e8d6af917c4595274d049aa2ed2c05f71
justsmileli <1195488604@qq.com>
2017-07-11 15:28:49 +0800

    左侧导航栏调整

--- src/main/webapp/assets/js/oj.js
+++ src/main/webapp/assets/js/oj.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
     if($("body").width()<1000){
         $("#oj-navbar").css("width",44);
         $("#oj-top-navbar").parent().css("position","fixed");
-        $(".main").css({"width":$("body").width()-40,"padding-left":"44px"});
+        $(".main").css({"width":$("body").width()-45,"padding-left":"45px"});
         $(".nav-pills a").each(function(){
             var text = $(this).html().split("i>")[0]+"i>";
             $(this).html(text).parent().css("padding","0");
