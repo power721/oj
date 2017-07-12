ca886fd3285748ae8ee854bbaefab55b92805108
justsmileli <1195488604@qq.com>
2017-07-12 17:02:24 +0800

    cprogram首页

---
+++ src/main/webapp/assets/js/cprogram.js
@@ -0,0 +1,9 @@
+/**
+ * Created by justsmileli on 2017/7/12.
+ */
+$(".cprog_btn").mouseover(function () {
+    $(this).css("opacity","1");
+}).mouseout(function () {
+    $(this).css("opacity","0.7");
+});
+
