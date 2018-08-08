cd92c1ec29af8c6967fef3cf9fb152ec97538dcf
上决╇ф <w703710691d@163.com>
2018-08-08 11:12:11 +0800

    mysql8.0

--- src/main/java/com/power/oj/util/model/DBConn.java
+++ src/main/java/com/power/oj/util/model/DBConn.java
@@ -1,12 +1,8 @@
 package com.power.oj.util.model;
 
-import com.mysql.jdbc.Statement;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
