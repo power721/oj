fcc87e5497b7b1c1be63d5b989582b2f9b7438a3
上决╇ф <w703710691d@163.com>
2018-08-24 20:57:04 +0800

    新增两个状态

--- src/main/java/com/power/oj/core/bean/ResultType.java
+++ src/main/java/com/power/oj/core/bean/ResultType.java
@@ -18,6 +18,8 @@ public class ResultType {
     public static final int RUN = 12;
     public static final int REJUDGE = 13;
     public static final int SIM = 14;
+    public static final int COM = 15;
+    public static final int QUE = 16;
     public static final int NOT_AC = 999;
 
     private int id;
