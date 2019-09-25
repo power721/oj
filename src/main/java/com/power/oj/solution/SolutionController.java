ae9b8894c34c762a1428a682f2a7eb1d373db570
上决╇ф <w703710691d@163.com>
2019-09-25 11:18:39 +0800

    fuck network center

--- src/main/java/com/power/oj/solution/SolutionController.java
+++ src/main/java/com/power/oj/solution/SolutionController.java
@@ -135,7 +135,7 @@ public class SolutionController extends OjController {
 
     @Before(POST.class)
     @RequiresPermissions("problem:submit") // code:add
-    public void save() {
+    public void get404Page() {
         SolutionModel solutionModel = getModel(SolutionModel.class, "solution");
         solutionModel.set("uid", userService.getCurrentUid());
         Integer cid = solutionModel.getInt("cid");
